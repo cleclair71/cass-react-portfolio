9dc55555c00690592f76ef9fdd54e62f451642d7
cass <76407605+cleclair71@users.noreply.github.com>
2023-05-10 16:41:00 -0400

    navbar font animation color

--- src/components/Banner/Header.jsx
+++ src/components/Banner/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
     return (
         <Container bar={bar}>
             <Logo>
-                <span className="green"><GiPaintBrush /></span>
+                <span className="black"><GiPaintBrush /></span>
                 <h1>My Portfolio</h1>
             </Logo>
             <Nav bar={bar}>
@@ -15,10 +15,10 @@ const Header = () => {
                 <span><a href="#service">Services</a></span>
                 <span><a href="#project">Projects</a></span>
                 <span><a href="#footer">Portfolio</a></span>
-                </Nav>
-                <div onClick={() => setBar(!bar)} className="bars">
-                    <div className="bar"></div>
-                    </div>
+            </Nav>
+            <div onClick={() => setBar(!bar)} className="bars">
+                <div className="bar"></div>
+            </div>
         </Container>
     )
 }
@@ -26,5 +26,57 @@ const Header = () => {
 export default Header;
 
 const Container = styled.div``
-const Logo = styled.div``
-const Nav = styled.div``
+const Logo = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    span {
+        font-size: 2rem;
+        }
+        h1 {
+            font-size: 1.5rem;
+            font-weight: 600;`
+const Nav = styled.div`
+@media (max-width: 640px) {
+    position: fixed;
+    display: flex;
+    flex-direction: column;
+    background-color: var(--background);
+    inset: 0;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+    font-weight: 700;
+    height: 100vh;
+    z-index: 100;
+    transition: height 400ms ease-in-out;
+}
+span {
+    margin-left: 1rem;
+    a {
+        color: var(--black);
+        text-decoration: none;
+        font-family: var(--hand);
+        position: relative;
+        font-weight: 300;
+        :before {
+            content: "";
+            position: absolute;
+            left: 0;
+            right: 0;
+            bottom: -2px;
+            height: 2px;
+            background-color: var(--black);
+            transform: scaleX(0);
+            transform-origin: right;
+            transition: transform 400ms ease-in-out;
+            }
+            :hover:before {
+                transform: scaleX(1);
+                transform-origin: left;
+                }
+                :hover {
+                    opacity: 0.8;
+                    }
+                    }
+                    }`
