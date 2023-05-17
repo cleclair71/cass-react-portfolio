5323215e1893e329871a3ccdeb070d89e2d2d659
Cassie LeClair <cassieleclair71@gmail.com>
2023-05-17 15:41:48 -0400

    resume download

--- src/components/resume/ResHeader.jsx
+++ src/components/resume/ResHeader.jsx
@@ -8,21 +8,21 @@ const ResHeader = () => {
         <Container id="home">
             <Slide direction="left">
                 <Texts>
-                <h1>I'm Cassie LeClair.</h1>
+                    <h1>I'm Cassie LeClair.</h1>
                     <p>I'm a Front End Web Developer based in the Greater Toronto Area. I have a passion for creating beautiful, responsive websites and applications. I'm currently seeking a full-time position as a Front End Web Developer.</p>
                     <Buttons>
                         <button>Contact</button>
-                        <button>
+                        <a href="/assets/Resume/resume.pdf" download>
                             Resume <AiOutlineDownload />
-                        </button>
+                        </a>
                     </Buttons>
-                    
+
                 </Texts>
             </Slide>
         </Container>
     );
 };
-    
+
 export default ResHeader;
 
 const Container = styled.div`
@@ -69,34 +69,60 @@ p {
     }
     
 }`;
-const Buttons = styled.div`
+
+    const Buttons = styled.div`
 margin-top: 2rem;
+display: flex;
+gap: 1rem;
+flex-direction: row;
+flex-wrap: wrap;
+justify-content: space-between;
+button {
+    background: none;
+    border: 1px solid var(--white);
+    padding: 0.7rem 1.4rem;
+    cursor: pointer;
+    font-weight: 600;
+    filter: drop-shadow(0px 8px 8px #13262a);
+    color: var(--white);
+    font-family: 'Montserrat', sans-serif;
+    letter-spacing: 0.2rem;
+    font-size: .8rem;
+    text-transform: uppercase;
     display: flex;
-    gap: 1rem;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: space-between;
-    button {
-        background: none;
-        border: 1px solid var(--white);
-        padding: 0.7rem 1.4rem;
-        cursor: pointer;
-        font-weight: 600;
-        filter: drop-shadow(0px 8px 8px #13262a);
-        color: var(--white);
-        font-family: 'Montserrat', sans-serif;
-        letter-spacing: 0.2rem;
-        font-size: .8rem;
-        text-transform: uppercase;
-        display: flex;
-        align-items: center;
-        gap: 0.5rem;
-        border-radius: 5px;
-        @media (max-width: 640px) {
-            padding: 0.5rem 1rem;
-            font-size: 0.7rem;
-        }
-        :hover {
-            background-color: rgba(255, 248, 244, 0.2);
-        }
-    }`;
+    align-items: center;
+    gap: 0.5rem;
+    border-radius: 5px;
+    @media (max-width: 640px) {
+        padding: 0.5rem 1rem;
+        font-size: 0.7rem;
+    }
+    :hover {
+        background-color: rgba(255, 248, 244, 0.2);
+    }
+}
+a {
+    background: none;
+    border: 1px solid var(--white);
+    padding: 0.7rem 1.4rem;
+    cursor: pointer;
+    font-weight: 600;
+    filter: drop-shadow(0px 8px 8px #13262a);
+    color: var(--white);
+    font-family: 'Montserrat', sans-serif;
+    letter-spacing: 0.2rem;
+    font-size: .8rem;
+    text-transform: uppercase;
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    border-radius: 5px;
+    text-decoration: none; // This removes the underline from the link
+    @media (max-width: 640px) {
+        padding: 0.5rem 1rem;
+        font-size: 0.7rem;
+    }
+    :hover {
+        background-color: rgba(255, 248, 244, 0.2);
+    }
+}`;
